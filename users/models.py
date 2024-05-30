f5fc45b57fe252b96281cd9d6cb383ed4a3a4148
panthurr0 <olapopaola@mail.ru>
2024-05-30 15:18:32 +0300

    create app users

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    avatar = models.ImageField(verbose_name='аватар', upload_to='users/', **NULLABLE)
+    phone_number = models.CharField(max_length=25, verbose_name='номер телефона', **NULLABLE)
+
+    token = models.CharField(max_length=100, verbose_name='Token', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
