5f6cc281c40969d911796844e4e429bcb6044aee
panthurr0 <olapopaola@mail.ru>
2024-06-05 12:28:10 +0300

    add crontab command

---
+++ mailing/management/commands/run_mailing.py
@@ -0,0 +1,17 @@
+import logging
+
+import os
+import django
+from django.core.management.base import BaseCommand
+
+from mailing.crontab import do
+
+logger = logging.getLogger(__name__)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+django.setup()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # Вызываем команду напрямую
+        do()
