84c1b9cd4d2d412c6cd8382307bc3424ce7360cc
panthurr0 <olapopaola@mail.ru>
2024-06-05 23:35:54 +0300

    add fixtures

--- mailing/management/commands/fill.py
+++ mailing/management/commands/fill.py
@@ -4,8 +4,7 @@ import pathlib
 from django.core.management import BaseCommand
 from django.db import connection
 
-from blog.models import Blog
-from catalog.models import Category, Product, Version
+from mailing.models import Client, MailingText, Mailing, Status
 from users.models import User
 
 ROOT = pathlib.Path(__file__).parent.parent.parent.parent
@@ -13,9 +12,6 @@ DATA_CLIENT = pathlib.Path(ROOT, 'json_data', 'client.json')
 DATA_MAILINGTEXT = pathlib.Path(ROOT, 'json_data', 'mailingtext.json')
 DATA_MAILING = pathlib.Path(ROOT, 'json_data', 'mailing.json')
 DATA_STATUS = pathlib.Path(ROOT, 'json_data', 'status.json')
-DATA_MAILING = pathlib.Path(ROOT, 'json_data', 'mailing.json')
-DATA_MAILING = pathlib.Path(ROOT, 'json_data', 'mailing.json')
-
 
 
 class Command(BaseCommand):
@@ -29,63 +25,60 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Очистка базы данных перед заполнением
-        Category.objects.all().delete()
-        Product.objects.all().delete()
-        Version.objects.all().delete()
-        Blog.objects.all().delete()
+        Client.objects.all().delete()
+        MailingText.objects.all().delete()
+        Mailing.objects.all().delete()
+        Status.objects.all().delete()
 
-        category_for_create = []
-        product_for_create = []
-        version_for_create = []
-        blog_for_create = []
+        client_for_create = []
+        mailingtext_for_create = []
+        mailing_for_create = []
+        status_for_create = []
 
         with connection.cursor() as cursor:
             cursor.execute(
-                "TRUNCATE TABLE catalog_category, catalog_product, catalog_version, "
-                "blog_blog RESTART IDENTITY CASCADE;")
+                "TRUNCATE TABLE mailing_client, mailing_mailingtext, mailing_mailing,"
+                "mailing_status RESTART IDENTITY CASCADE;")
 
-        # Заполнение категорий
-        for category in Command.json_read(DATA_CATEGORY):
-            category_fields = category.get('fields')
-            category_for_create.append(
-                Category(category_title=category_fields.get('category_title'),
-                         category_description=category_fields.get('category_description'))
+        for client in Command.json_read(DATA_CLIENT):
+            client_fields = client.get('fields')
+            client_for_create.append(
+                Client(name=client_fields.get('name'),
+                       email=client_fields.get('email'),
+                       comment=client_fields.get('comment'))
             )
 
-        Category.objects.bulk_create(category_for_create)
+        Client.objects.bulk_create(client_for_create)
 
-        # Заполнение продуктов
-        for product in Command.json_read(DATA_PRODUCT):
-            product_fields = product.get('fields')
-            product_for_create.append(
-                Product(product_title=product_fields.get('product_title'),
-                        category=Category.objects.get(pk=product_fields.get('category')),
-                        price=product_fields.get('price'),
-                        owner=User.objects.get(pk=product_fields.get('owner')),
-                        is_active=product_fields.get('is_active'),
-                        product_description=product_fields.get('product_description'),
-                        image=product_fields.get('image'))
+        for mailingtext in Command.json_read(DATA_MAILINGTEXT):
+            mailingtext_fields = mailingtext.get('fields')
+            mailingtext_for_create.append(
+                MailingText(theme=mailingtext_fields.get('theme'),
+                            body=mailingtext_fields.get('body'))
             )
-        Product.objects.bulk_create(product_for_create)
+        MailingText.objects.bulk_create(mailingtext_for_create)
 
-        # Заполнение версий
-        for version in Command.json_read(DATA_VERSIONS):
-            version_fields = version.get('fields')
-            version_for_create.append(
-                Version(product=Product.objects.get(pk=version_fields.get('product')),
-                        version_number=version_fields.get('version_number'),
-                        version_title=version_fields.get('version_title'),
-                        is_active=version_fields.get('is_active'))
+        for mailing in Command.json_read(DATA_MAILING):
+            mailing_fields = mailing.get('fields')
+            mailing_for_create.append(
+                Mailing(start_time=mailing_fields.get('start_time'),
+                        end_time=mailing_fields.get('end_time'),
+                        frequency=mailing_fields.get('frequency'),
+                        status_of_mailing=mailing_fields.get('status_of_mailing'),
+                        is_active=mailing_fields.get('is_active'),
+                        owner=User.objects.get(pk=mailing_fields.get('owner')),
+                        clients=Client.objects.filter(pk__in=mailing_fields.get('clients')),
+                        mail=MailingText.objects.filter(pk__in=mailing_fields.get('mail')),)
             )
-        Version.objects.bulk_create(version_for_create)
+        Mailing.objects.bulk_create(mailing_for_create)
 
-        # Заполнение блогов
-        for blog in Command.json_read(DATA_BLOG):
-            blog_fields = blog.get('fields')
-            blog_for_create.append(
-                Blog(title=blog_fields.get('title'),
-                     content=blog_fields.get('content'),
-                     preview=blog_fields.get('preview'),
-                     is_published=blog_fields.get('is_published'))
+        for status in Command.json_read(DATA_STATUS):
+            status_fields = status.get('fields')
+            status_for_create.append(
+                Status(last_attempt=status_fields.get('last_attempt'),
+                       last_attempt_status=status_fields.get('last_attempt_status'),
+                       client=User.objects.get(pk=status_fields.get('client')),
+                       mailing_list=Mailing.objects.get(pk=status_fields.get('mailing_list')),
+                       server_response=status_fields.get('server_response'))
             )
-        Blog.objects.bulk_create(blog_for_create)
+        Status.objects.bulk_create(status_for_create)
